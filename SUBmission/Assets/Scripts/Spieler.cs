7e9e0909e09d359bad400a84e2ee74a21db30f01
schto031 <scholertom@gmail.com>
2017-12-19 16:01:22 +0100

    Added initial project
    Initial project + basic movement of the Submarine

---
+++ SUBmission/Assets/Scripts/Spieler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spieler : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	float eingabeFaktor = 8;
+	public GameObject[] geschoss = new GameObject[3];
+
+	//  Update is called once per frame
+	void Update () {
+
+	
+
+		//Eingabe speichern
+		float xEingabe = Input.GetAxis ("Horizontal");
+		float yEingabe = Input.GetAxis ("Vertical");
+
+		//Neue Position bestimmen
+
+		float xNeu = transform.position.x + xEingabe * eingabeFaktor* Time.deltaTime ;
+		float yNeu = transform.position.y + yEingabe * eingabeFaktor* Time.deltaTime ;
+
+
+
+
+
+		transform.position = new Vector2 (xNeu, yNeu);
+	}
+}
