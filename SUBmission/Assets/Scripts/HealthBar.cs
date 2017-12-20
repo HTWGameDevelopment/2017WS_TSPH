f8076fdc62e8f716418c05462e67fcf86e589987
Peter191 <peter-hennen-191@hotmail.de>
2017-12-20 15:24:11 +0100

    Healthbar
    healthbar added

---
+++ SUBmission/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+	public GameObject healthBar;
+	public float Max_Hp = 100f;
+	public float current_Hp = 0f;
+	float clacHealth=0f;
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transformHealth ();
+	}
+	public void transformHealth ()
+	{
+		clacHealth = current_Hp / Max_Hp;
+		healthBar.transform.localScale = new Vector3(clacHealth, 1, 1);
+	}
+}
