d06d9d3fc685333ed4ade437492d33dfd9a23e6a
Peter191 <peter-hennen-191@hotmail.de>
2018-01-30 12:22:47 +0100

    merge
    sollte jetzt funktionieren

---
+++ SUBmission/Assets/Scripts/StatValues.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatValues : MonoBehaviour {
+    //SielerStats
+    public int money = 0;
+    //Position
+    public int LevelIteration = 0;
+    public int level;
+    //UpgradeLvl
+    public int Geschoss_Dmg;
+    public int Geschoss_Count;
+    public int Geschoss_Speed;
+    public int Torpedo_Reserve;
+    public int Torpedo_Dmg;
+    public int Torpedo_Regeneration;
+    public int Torpedo_Dot;
+    public int Torpedo_Tickrate;
+    public int Hp_Max;
+    public int Hp_Regeneration;
+    public int Player_Speed;
+    public bool CloseLeftBorder;
+
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- SUBmission/Assets/Scripts/Upgrade.cs
+++ SUBmission/Assets/Scripts/Upgrade.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpGreate : MonoBehaviour {
+public class UpGreate : MonoBehaviour
+{
     public int Geschoss_Dmg;
     public int Geschoss_Count;
     public int Geschoss_Speed;
@@ -15,13 +16,27 @@ public class UpGreate : MonoBehaviour {
     public int Hp_Regeneration;
     public int Player_Speed;
     public bool CloseLeftBorder;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        Geschoss_Dmg = 0;
+        Geschoss_Count = 0;
+        Geschoss_Speed = 0;
+        Torpedo_Reserve = 0;
+        Torpedo_Dmg = 0;
+        Torpedo_Regeneration = 0;
+        Torpedo_Dot = 0;
+        Torpedo_Tickrate = 0;
+        Hp_Max = 0;
+        Hp_Regeneration = 0;
+        Player_Speed = 0;
+        CloseLeftBorder = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
