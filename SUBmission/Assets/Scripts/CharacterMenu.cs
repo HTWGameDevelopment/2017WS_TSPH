9e74c7acf3ef6ed3e173c70b1a2098a4d88c9824
schto031 <scholertom@gmail.com>
2018-01-30 15:06:39 +0100

    Database

---
+++ SUBmission/Assets/Scripts/CharacterMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMenu : MonoBehaviour {
+
+
+	public Text coins;
+	public Text maxHealth;
+	public Text maxTorpedo;
+	public Text maxAmmo;
+	public GameObject SpielerReferance;
+
+
+	// Use this for initialization
+	void Start () {
+		coins.text = SpielerReferance.GetComponent<Spieler>().Coins.ToString();
+		maxHealth.text = SpielerReferance.GetComponent<Spieler>().Max_Hp.ToString();
+		maxTorpedo.text= SpielerReferance.GetComponent<Spieler>().MaxTorpedos.ToString();
+		maxAmmo.text= SpielerReferance.GetComponent<Spieler>().MaxGeschossCount.ToString();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ SUBmission/Assets/Scripts/DataScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataScript : MonoBehaviour {
+
+
+
+	public int Max_HpLevel = 0;
+	public int Coins = 0;
+	public int GeschossLevel = 0;
+	public int TorpedosLevel = 0;
+	public Button upgradeHealthButton;
+	public Button upgradeTorpedoButton;
+	public Button upgradeAmmoButton;
+	//public float Geschoss_Dmg=10f;
+	//public float Torpedo_Dmg = 20f;
+	//public float Explosion_Dmg = 50f;
+	//public float Explosion_DmgDelay = 0.1f;
+	//public float Explosion_DotDmg = 10f;
+	//public float Explosion_Tickrate = 0.2f;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		upgradeAmmoButton.onClick.AddListener (upgradeAmmoButton_Click);
+		upgradeHealthButton.onClick.AddListener (upgradeHealthButton_Click);
+		upgradeTorpedoButton.onClick.AddListener (upgradeTorpedoButton_Click);
+	}
+
+	public void  upgradeHealthButton_Click(){
+		Max_HpLevel ++;
+	}
+	public void  upgradeTorpedoButton_Click(){
+		TorpedosLevel ++;
+	}
+	public void  upgradeAmmoButton_Click(){
+		GeschossLevel ++;
+	}
+
+}
